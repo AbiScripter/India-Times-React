cf2702a5dc5cb6ae7708d79d29d2ca13260cc298
Abilash <abilashb2000@gmail.com>
2024-05-18 08:52:10 +0530

    cleared errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import "./App.css";
 import { useEffect, useState } from "react";
 import Loader from "./components/Loader";
 import CategoryTabs from "./components/CategoryTabs";
